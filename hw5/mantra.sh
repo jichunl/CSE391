689a03c7c38aed3043cc60a708102211e5efb442
jichunl <jichunli@uw.edu>
2018-07-24 22:21:10 -0700

    Correction on mantra.sh

--- hw5/mantra.sh
+++ hw5/mantra.sh
@@ -3,16 +3,17 @@
 # Course: CSE 391 Summer 2018
 # Student ID: 1531264
 
-read STR_ARG TIMES 
-LENGTH=$(wc -m <<< $STR_ARG)
+ 
+LENGTH=$(wc -m <<< $1)
 printf "**"
 for char in $(seq $LENGTH); do
 	printf "*";
 done
+
 printf "*\n"
-for num in $(seq $TIMES); do
+for num in $(seq $2); do
 	printf "* "
-	printf "$STR_ARG"
+	printf "$1"
 	printf " *\n"
 done
 
